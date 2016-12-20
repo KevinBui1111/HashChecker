c25895ef3113a48734eaeff0ddd0d1c2e07b516d
khanh.buidang <khanh.buidang@VNHQDEV01>
2016-12-20 18:13:25 +0700

    Working

---
+++ frmMain.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace HashChecker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.olvFiles = new BrightIdeasSoftware.ObjectListView();
+            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.lbDir = new System.Windows.Forms.Label();
+            this.btnCheckDup = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.olvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // olvFiles
+            // 
+            this.olvFiles.AllColumns.Add(this.olvColumn1);
+            this.olvFiles.AllColumns.Add(this.olvColumn2);
+            this.olvFiles.AllColumns.Add(this.olvColumn3);
+            this.olvFiles.AllColumns.Add(this.olvColumn4);
+            this.olvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.olvFiles.CellEditUseWholeCell = false;
+            this.olvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3,
+            this.olvColumn4});
+            this.olvFiles.Cursor = System.Windows.Forms.Cursors.Default;
+            this.olvFiles.FullRowSelect = true;
+            this.olvFiles.GridLines = true;
+            this.olvFiles.Location = new System.Drawing.Point(12, 45);
+            this.olvFiles.Name = "olvFiles";
+            this.olvFiles.ShowGroups = false;
+            this.olvFiles.Size = new System.Drawing.Size(399, 298);
+            this.olvFiles.TabIndex = 1;
+            this.olvFiles.UseCompatibleStateImageBehavior = false;
+            this.olvFiles.View = System.Windows.Forms.View.Details;
+            this.olvFiles.FormatRow += new System.EventHandler<BrightIdeasSoftware.FormatRowEventArgs>(this.olvFiles_FormatRow);
+            // 
+            // olvColumn1
+            // 
+            this.olvColumn1.AspectName = "Name";
+            this.olvColumn1.Text = "Name";
+            this.olvColumn1.Width = 103;
+            // 
+            // olvColumn2
+            // 
+            this.olvColumn2.AspectName = "Checksum";
+            this.olvColumn2.AspectToStringFormat = "";
+            this.olvColumn2.Text = "Hash";
+            this.olvColumn2.Width = 84;
+            // 
+            // olvColumn3
+            // 
+            this.olvColumn3.AspectName = "Status";
+            this.olvColumn3.Text = "Status";
+            // 
+            // olvColumn4
+            // 
+            this.olvColumn4.AspectName = "Dir";
+            this.olvColumn4.AspectToStringFormat = "";
+            this.olvColumn4.Text = "Directory";
+            this.olvColumn4.Width = 89;
+            // 
+            // lbDir
+            // 
+            this.lbDir.AutoSize = true;
+            this.lbDir.Location = new System.Drawing.Point(12, 18);
+            this.lbDir.Name = "lbDir";
+            this.lbDir.Size = new System.Drawing.Size(38, 15);
+            this.lbDir.TabIndex = 5;
+            this.lbDir.Text = "label1";
+            // 
+            // btnCheckDup
+            // 
+            this.btnCheckDup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheckDup.Location = new System.Drawing.Point(255, 12);
+            this.btnCheckDup.Name = "btnCheckDup";
+            this.btnCheckDup.Size = new System.Drawing.Size(75, 27);
+            this.btnCheckDup.TabIndex = 7;
+            this.btnCheckDup.Text = "Check Dup";
+            this.btnCheckDup.UseVisualStyleBackColor = true;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Location = new System.Drawing.Point(336, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 27);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 349);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(399, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 384);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCheckDup);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lbDir);
+            this.Controls.Add(this.olvFiles);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.olvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.ObjectListView olvFiles;
+        private BrightIdeasSoftware.OLVColumn olvColumn1;
+        private BrightIdeasSoftware.OLVColumn olvColumn2;
+        private BrightIdeasSoftware.OLVColumn olvColumn4;
+        private BrightIdeasSoftware.OLVColumn olvColumn3;
+        private System.Windows.Forms.Label lbDir;
+        private System.Windows.Forms.Button btnCheckDup;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,170 @@
+﻿using KevinHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static KevinHelper.HashCalculator;
+
+namespace HashChecker
+{
+    public partial class frmMain : Form
+    {
+        const string record_pattern_crc32 = @"^(\w{8}) \*(.+)$";
+        const string record_pattern_md5 = @"^(\w{32}) \*(.+)$";
+        const string record_pattern_sha1 = @"^(\w{40}) \*(.+)$";
+
+        AlgorithmType algorithmType;
+        List<FileCheck> listInput = new List<FileCheck>();
+        Dictionary<FileCheck.FileStatus, Color> colorStatus;
+
+        string record_pattern;
+        string file_checksum;
+        public frmMain()
+        {
+            InitializeComponent();
+
+            colorStatus = new Dictionary<FileCheck.FileStatus, Color>();
+            colorStatus[FileCheck.FileStatus.NONE] = Color.Black;
+            colorStatus[FileCheck.FileStatus.SUCCESS] = Color.RoyalBlue;
+            colorStatus[FileCheck.FileStatus.FAILED] = Color.Red;
+            colorStatus[FileCheck.FileStatus.NOTFOUND] = Color.Gray;
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            file_checksum = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+
+            bool check = CheckInput();
+            if (check) PopularInput();
+        }
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            string file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            bool is_file = File.Exists(file);
+            if (is_file && e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private async void btnCheck_Click(object sender, EventArgs e)
+        {
+            HashCalculator hcal = new HashCalculator();
+            hcal.algorithm = algorithmType;
+            hcal.files = listInput.Select(f => f.full_path).ToList();
+            hcal.progressCallback += Hcal_progressCallback;
+            hcal.resultCallback += Hcal_resultCallback;
+
+            await hcal.ComputeHashAsync();
+
+        }
+
+        private void Hcal_resultCallback(string checksum, int index, Exception ex)
+        {
+            var file = listInput[index];
+            if (ex != null)
+            {
+                Console.WriteLine(ex.Message);
+                file.Status = FileCheck.FileStatus.NOTFOUND;
+            }
+            else
+            {
+                file.Checksum_new = checksum;
+                file.Status = file.Checksum_new == file.Checksum ? FileCheck.FileStatus.SUCCESS : FileCheck.FileStatus.FAILED;
+            }
+
+            olvFiles.RefreshObject(file);
+            olvFiles.EnsureModelVisible(file);
+        }
+
+        private void Hcal_progressCallback(string file_name, int percent)
+        {
+            progressBar1.Value = percent;
+            this.Text = string.Format("{0}% - {1}", percent, Path.GetFileName(file_name));
+        }
+
+        bool CheckInput()
+        {
+            string line;
+            using (var stream = File.OpenText(file_checksum)) { line = stream.ReadLine(); }
+
+            algorithmType = AlgorithmType.NONE;
+
+            if (Regex.IsMatch(line, record_pattern_crc32))
+            {
+                record_pattern = record_pattern_crc32;
+                algorithmType = AlgorithmType.CRC32;
+            }
+            else if (Regex.IsMatch(line, record_pattern_md5))
+            {
+                record_pattern = record_pattern_md5;
+                algorithmType = AlgorithmType.MD5;
+            }
+            else if (Regex.IsMatch(line, record_pattern_sha1))
+            {
+                record_pattern = record_pattern_sha1;
+                algorithmType = AlgorithmType.SHA1;
+            }
+
+            if (algorithmType != AlgorithmType.NONE)
+                lbDir.Text = algorithmType + " - " + file_checksum;
+            else
+                lbDir.Text = "Error!";
+
+            return algorithmType != AlgorithmType.NONE;
+        }
+        void PopularInput()
+        {
+            string folder = Path.GetDirectoryName(file_checksum);
+            listInput.Clear();
+            using (var stream = File.OpenText(file_checksum))
+            {
+                while (!stream.EndOfStream)
+                {
+                    string line = stream.ReadLine();
+                    Match m = Regex.Match(line, record_pattern);
+                    if (m.Success)
+                    {
+                        listInput.Add(new FileCheck
+                        {
+                            full_path =  Path.Combine(folder, m.Groups[2].Value),
+                            Checksum = m.Groups[1].Value
+                        });
+                    }
+                }
+            }
+
+            olvFiles.SetObjects(listInput);
+        }
+
+        private void olvFiles_FormatRow(object sender, BrightIdeasSoftware.FormatRowEventArgs e)
+        {
+            FileCheck file = (FileCheck)e.Model;
+            e.Item.ForeColor = colorStatus[file.Status];
+        }
+    }
+
+    class FileCheck
+    {
+        public string full_path { get; set; }
+        public string Name { get { return Path.GetFileName(full_path); } }
+        public string Dir { get { return Path.GetDirectoryName(full_path); } }
+
+        public string Checksum { get; set; }
+        public string Checksum_new { get; set; }
+        public FileStatus Status { get; set; }
+
+        public enum FileStatus
+        {
+            NONE,
+            SUCCESS,
+            FAILED,
+            NOTFOUND
+        }
+    }
+}
